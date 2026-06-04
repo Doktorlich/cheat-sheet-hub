f4900f0dfe10f7e806573fa074b1e05fca08d3d4
Doktorlich <sergei199522@mail.ru>
2026-06-04 19:33:57 +0300

    style: add style for the sheetSlug

--- app/[sheetSlug]/page.tsx
+++ app/[sheetSlug]/page.tsx
@@ -2,9 +2,8 @@ import classes from "./page.module.css";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getSheet } from "@/app/lib/actions";
-// Импортируем новый тип subcategory вместо старого sheet
 import { ELEMENTS_LIST, subcategory } from "@/app/lib/mockData";
-import CodeBlock from "@/app/components/builder/CodeBlock";
+import Subcategories from "@/app/components/categories/Subcategories";
 
 type Props = {
     params: Promise<{ sheetSlug: string }>;
@@ -22,44 +21,11 @@ export default async function SheetPage({ params }: Props) {
         notFound();
     }
 
-    const [sheetFirstElem] = ELEMENTS_LIST.filter(e => e.category === sheetSlug);
-    // Достаем массив подкатегорий subcategory вместо старого sheet
-    const subcategories: subcategory[] | undefined = sheetFirstElem.subcategory;
-
-    if (!subcategories) {
-        throw new Error("subcategories not found");
-    }
-
     return (
         <main className={classes.container}>
             <h1 className={classes.h1}>{sheetSlug}</h1>
 
-            {/* 1-й MAP: Перебираем блоки подкатегорий (hooks, custom hooks) */}
-            {subcategories.map(sub => {
-                return (
-                    <section key={sub.title} className={classes.section}>
-                        <h2 className={classes.subcategoryTitle}>{sub.title}</h2>
-
-                        <ul className={classes["sheets-list"]}>
-                            {/* 2-й MAP: Перебираем карточки внутри текущей подкатегории */}
-                            {sub.sheet.map(item => {
-                                return (
-                                    <li key={item.shortName}>
-                                        <h3>{item.shortName}</h3>
-                                        <div className={classes["code-block"]}>
-                                            <div className={classes.code}>
-                                                <CodeBlock code={String(item.code)} language={"jsx"} />
-                                                <button type={"button"}></button>
-                                            </div>
-                                        </div>
-                                        <p>{item.description}</p>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </section>
-                );
-            })}
+            <Subcategories sheetSlug={sheetSlug} />
         </main>
     );
 }
