035a843afe441357f9c2d54e715c5dbcf20e0c31
Doktorlich <sergei199522@mail.ru>
2026-06-04 19:36:20 +0300

    refactor: extract subcategory logic out of HomePage

---
+++ app/components/categories/Subcategories.tsx
@@ -0,0 +1,47 @@
+import CodeBlock from "@/app/components/builder/CodeBlock";
+import { ELEMENTS_LIST, subcategory } from "@/app/lib/mockData";
+import classes from "./Subcategories.module.css";
+
+interface SubcategoriesProps {
+    sheetSlug: string;
+}
+
+export default function Subcategories({ sheetSlug }: SubcategoriesProps) {
+    const [sheetFirstElem] = ELEMENTS_LIST.filter(e => e.category === sheetSlug);
+    // Достаем массив подкатегорий subcategory вместо старого sheet
+    const subcategories: subcategory[] | undefined = sheetFirstElem.subcategory;
+
+    if (!subcategories) {
+        throw new Error("subcategories not found");
+    }
+    return (
+        <>
+            {/* 1-й MAP: Перебираем блоки подкатегорий (hooks, custom hooks) */}
+            {subcategories.map(sub => {
+                return (
+                    <section key={sub.title} className={classes.section}>
+                        <h2 className={classes.subcategoryTitle}>{sub.title}</h2>
+
+                        <ul className={classes["sheets-list"]}>
+                            {/* 2-й MAP: Перебираем карточки внутри текущей подкатегории */}
+                            {sub.sheet.map(item => {
+                                return (
+                                    <li key={item.shortName}>
+                                        <h3>{item.shortName}</h3>
+                                        <div className={classes["code-block"]}>
+                                            <div className={classes.code}>
+                                                <CodeBlock code={String(item.code)} language={"jsx"} />
+                                                <button type={"button"}></button>
+                                            </div>
+                                        </div>
+                                        <p>{item.description}</p>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </section>
+                );
+            })}
+        </>
+    );
+}
